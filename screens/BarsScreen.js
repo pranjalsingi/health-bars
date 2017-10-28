cc36da968b6424395d709dde3073ffc0de3b82e8
mirdathebirda <mirandachiu13@gmail.com>
2017-10-28 19:11:03 -0400

    bars screen now have actual bars

--- screens/BarsScreen.js
+++ screens/BarsScreen.js
@@ -11,15 +11,25 @@ export default class LinksScreen extends React.Component {
     return (
       <View style={styles.welcomeContainer}>
         <Text style={styles.getStartedText}>Individual health bars go on this page</Text>
+        <Text> Main Health Bar </Text>
+          <View style={styles.statusBar} />
+          <Text> Eating Bar  </Text>
+          <View style={styles.statusBar} />
+          <Text> Sleep Bar </Text>
+          <View style={styles.statusBar} />
+          <Text> Water Bar </Text>
+         <View style={styles.statusBar} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+  statusBar: {
+    backgroundColor: "#C2185B",
+    height: Constants.statusBarHeight,
+    width: Constants.statusBarWidth,
   },
+
+  // rest of the styles
 });
