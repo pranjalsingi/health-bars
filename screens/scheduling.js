deec60c92e144f71b0a0d622fdc0cdf615bbab6f
Jared Koester <jaredjkoester@gmail.com>
2017-10-28 22:18:14 -0400

    added scheduling.js, which will provide helper functions to schedule notifications

---
+++ screens/scheduling.js
@@ -0,0 +1,13 @@
+import { Notifications } from 'expo'
+
+function notificationCallback (notif) {
+	// STRING origin will be 'recieved' as soon as the notification arrives, and will be 'selected' when the user taps on the notification
+	origin = notif.origin
+	//JSON or maybe custom object. TBD it's up to us.
+	data = data
+
+}
+
+newDailyNotification (head, body, data) {
+
+}
